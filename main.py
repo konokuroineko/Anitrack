26815cf53612aaeea5a2e4c34a938d5da65335c2
kuroineko <296097903+konokuroineko@users.noreply.github.com>
2026-09-11 22:23:39 +0330

    Make startup window mode configurable

--- main.py
+++ main.py
@@ -3,12 +3,16 @@ import sys
 from PySide6.QtWidgets import QApplication
 
 from ui.main_window import MainWindow
+from ui.preferences import get
 
 
 def main():
     app = QApplication(sys.argv)
     window = MainWindow()
-    window.showMaximized()
+    if get("maximized"):
+        window.showMaximized()
+    else:
+        window.show()
     sys.exit(app.exec())
 
 
